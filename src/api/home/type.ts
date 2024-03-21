8410bae1b1359f63bed7380c35d1dc8a0582219f
harry <2062454635@qq.com>
2024-03-22 02:27:16 +0800

    定义首页模块TS数据类型

---
+++ src/api/home/type.ts
@@ -0,0 +1,72 @@
+// 定义首页模块TS数据类型
+
+// 响应数据
+export interface ResponseData {
+  code: number;
+  message: string;
+  ok: boolean;
+}
+// 已有医院
+export interface Hospital {
+  id: string;
+  createTime: string;
+  updateTime: string;
+  isDeleted: number;
+  param: {
+    hostypeString: string;
+    fullAddress: string;
+  };
+  hoscode: string;
+  hosname: string;
+  hostype: string;
+  provinceCode: string;
+  cityCode: string;
+  districtCode: string;
+  address: string;
+  logoData: string;
+  intro: null;
+  route: string;
+  status: number;
+  bookingRule: {
+    cycle: number;
+    releaseTime: string;
+    stopTime: string;
+    quitDay: number;
+    quitTime: string;
+    rule: Array<string>;
+  };
+}
+// 已有医院数组
+export type HospitalList = Hospital[];
+
+// 响应的医院数据
+export interface HospitalResponseData extends ResponseData {
+  data: {
+    content: HospitalList;
+    pageable: {
+      sort: {
+        sorted: boolean;
+        unsorted: boolean;
+        empty: boolean;
+      };
+      pageNumber: number;
+      pageSize: number;
+      offset: number;
+      paged: boolean;
+      unpaged: boolean;
+    };
+    totalPages: number;
+    totalElements: number;
+    last: boolean;
+    first: boolean;
+    sort: {
+      sorted: boolean;
+      unsorted: boolean;
+      empty: boolean;
+    };
+    numberOfElements: number;
+    size: number;
+    number: number;
+    empty: boolean;
+  };
+}
