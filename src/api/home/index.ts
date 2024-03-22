bf04ee9d65a7fe4e14899811b041c91b8a235844
harry <2062454635@qq.com>
2024-03-23 00:13:49 +0800

    add: get level and region

--- src/api/home/index.ts
+++ src/api/home/index.ts
@@ -1,14 +1,26 @@
 import request from '@/utils/request';
 
-import type { HospitalResponseData } from './type';
+import type { HospitalResponseData, HospitalLevelAndRegionResponseData } from './type';
 
 // 统一管理接口地址
-enum Api {
+enum API {
+  // 获取医院列表
   // GET /api/hosp/hospital/{page}/{limit}
   HOSPITAL_URL = '/hosp/hospital/',
+
+  // 获取医院等级或地区
+  // GET /api/cmn/dict/findByDictCode/{dictCode}
+  HospitalLevelAndRegion_URL = '/cmn/dict/findByDictCode/',
 }
 
 // 获取医院列表
 export const getHospital = (page: number, limit: number) => {
-  return request.get<any, HospitalResponseData>(Api.HOSPITAL_URL + page + '/' + limit);
+  return request.get<any, HospitalResponseData>(API.HOSPITAL_URL + page + '/' + limit);
+};
+
+// 获取医院等级或地区
+export const reqHospitalLevelAndRegion = (dictCode: string) => {
+  return request.get<any, HospitalLevelAndRegionResponseData>(
+    API.HospitalLevelAndRegion_URL + dictCode
+  );
 };
