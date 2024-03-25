95741be47f5f4172dcb1586924a1f282e48cb957
harry <2062454635@qq.com>
2024-03-26 00:00:06 +0800

    整理

--- src/components/hospital_top/index.vue
+++ src/components/hospital_top/index.vue
@@ -1,63 +1,31 @@
+<script setup lang="ts">
+// 引入路由
+import { useRouter } from 'vue-router';
+const router = useRouter();
+// 点击回到首页
+const goHome = () => {
+  router.push('/home');
+  // console.log('点击了返回首页');
+};
+</script>
 <template>
-  <div class="top">
-    <div class="content">
-      <div class="left">
-        <img src="../../assets/images/logo.png" alt="logo" />
-        <h1>尚医通 预约挂号统一平台</h1>
+  <div class="fixed z-[999] w-full h-[70px] bg-white">
+    <div class="flex justify-between w-[1200px] h-[70px] mx-auto my-0 bg-white">
+      <!-- 左侧 -->
+      <div @click="goHome" class="flex items-center">
+        <img
+          src="../../assets/images/logo.png"
+          alt="logo"
+          class="w-[50px] h-[50px] mr-3 cursor-pointer" />
+        <h1 class="text-[20px] text-blue-400 cursor-pointer">尚医通 预约挂号统一平台</h1>
       </div>
-      <div class="right">
-        <p class="help">帮助中心</p>
-        <p class="login">登录/注册</p>
+      <!-- 右侧 -->
+      <div class="flex items-center">
+        <p class="ml-3 text-[15px] text-gray-500 cursor-pointer hover:text-blue-400">帮助中心</p>
+        <p class="ml-3 text-[15px] text-gray-500 cursor-pointer hover:text-blue-400">登录/注册</p>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
-
-<style scoped lang="scss">
-.top {
-  position: fixed;
-  z-index: 999;
-  width: 100%;
-  height: 70px;
-  background: #fff;
-
-  .content {
-    width: 1200px;
-    height: 70px;
-    margin: 0 auto;
-    background: #fff;
-    display: flex;
-    justify-content: space-between;
-
-    .left {
-      display: flex;
-      align-items: center;
-      img {
-        width: 50px;
-        height: 50px;
-        margin-right: 10px;
-      }
-      h1 {
-        font-size: 20px;
-        color: #55a6fe;
-      }
-    }
-
-    .right {
-      display: flex;
-      align-items: center;
-      p {
-        margin-left: 10px;
-        font-size: 14px;
-        color: #999;
-        cursor: pointer;
-        &:hover {
-          color: #55a6fe;
-        }
-      }
-    }
-  }
-}
-</style>
+<style scoped></style>
