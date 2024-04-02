eb73acccdcde95b1d4edd2264d14d2bd73bc99d5
harry <2062454635@qq.com>
2024-04-03 02:13:01 +0800

    add:pinia

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ import ElementPlus from 'element-plus'; // 引入ElementPlus
 import 'element-plus/dist/index.css';
 // @ts-ignore
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
+// 引入pinia仓库
+import pinia from '@/store/index';
 
 const app = createApp(App); // 创建应用实例
 
@@ -27,5 +29,6 @@ app.use(ElementPlus); // 使用ElementPlus
 app.use(ElementPlus, {
   locale: zhCn,
 }); // 使用ElementPlus中文
+app.use(pinia); // 使用pinia仓库
 
 app.mount('#app'); // 挂载应用实例
