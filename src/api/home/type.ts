3623ee6c4bdb0bbd18baba86d7f6beb8edd5517f
harry <2062454635@qq.com>
2024-04-03 02:13:16 +0800

    add:hospitalDetail

--- src/api/home/type.ts
+++ src/api/home/type.ts
@@ -2,40 +2,40 @@
 
 // 响应-数据
 export interface ResponseData {
-    code: number;
-    message: string;
-    ok: boolean;
+  code: number;
+  message: string;
+  ok: boolean;
 }
 
 // 医院
 export interface Hospital {
-    id: string;
-    createTime: string;
-    updateTime: string;
-    isDeleted: number;
-    param: {
-        hostypeString: string;
-        fullAddress: string;
-    };
-    hoscode: string;
-    hosname: string;
-    hostype: string;
-    provinceCode: string;
-    cityCode: string;
-    districtCode: string;
-    address: string;
-    logoData: string;
-    intro: null;
-    route: string;
-    status: number;
-    bookingRule: {
-        cycle: number;
-        releaseTime: string;
-        stopTime: string;
-        quitDay: number;
-        quitTime: string;
-        rule: Array<string>;
-    };
+  id: string;
+  createTime: string;
+  updateTime: string;
+  isDeleted: number;
+  param: {
+    hostypeString: string;
+    fullAddress: string;
+  };
+  hoscode: string;
+  hosname: string;
+  hostype: string;
+  provinceCode: string;
+  cityCode: string;
+  districtCode: string;
+  address: string;
+  logoData: string;
+  intro: null;
+  route: string;
+  status: number;
+  bookingRule: {
+    cycle: number;
+    releaseTime: string;
+    stopTime: string;
+    quitDay: number;
+    quitTime: string;
+    rule: Array<string>;
+  };
 }
 
 // 医院数组
@@ -43,48 +43,48 @@ export type HospitalList = Hospital[];
 
 // 响应-医院数据
 export interface HospitalResponseData extends ResponseData {
-    data: {
-        content: HospitalList;
-        pageable: {
-            sort: {
-                sorted: boolean;
-                unsorted: boolean;
-                empty: boolean;
-            };
-            pageNumber: number;
-            pageSize: number;
-            offset: number;
-            paged: boolean;
-            unpaged: boolean;
-        };
-        totalPages: number;
-        totalElements: number;
-        last: boolean;
-        first: boolean;
-        sort: {
-            sorted: boolean;
-            unsorted: boolean;
-            empty: boolean;
-        };
-        numberOfElements: number;
-        size: number;
-        number: number;
+  data: {
+    content: HospitalList;
+    pageable: {
+      sort: {
+        sorted: boolean;
+        unsorted: boolean;
         empty: boolean;
+      };
+      pageNumber: number;
+      pageSize: number;
+      offset: number;
+      paged: boolean;
+      unpaged: boolean;
     };
+    totalPages: number;
+    totalElements: number;
+    last: boolean;
+    first: boolean;
+    sort: {
+      sorted: boolean;
+      unsorted: boolean;
+      empty: boolean;
+    };
+    numberOfElements: number;
+    size: number;
+    number: number;
+    empty: boolean;
+  };
 }
 
 // 医院等级和地区
 interface HospitalLevelAndRegion {
-    id: number;
-    createTime: string;
-    updateTime: string;
-    isDeleted: number;
-    param: {};
-    parentId: number;
-    name: string;
-    value: string;
-    dictCode: null;
-    hasChildren: boolean;
+  id: number;
+  createTime: string;
+  updateTime: string;
+  isDeleted: number;
+  param: {};
+  parentId: number;
+  name: string;
+  value: string;
+  dictCode: null;
+  hasChildren: boolean;
 }
 
 // 医院等级和地区数组
@@ -92,10 +92,10 @@ export type HospitalLevelAndRegionArr = HospitalLevelAndRegion[];
 
 // 响应-医院等级和地区数据
 export interface HospitalLevelAndRegionResponseData extends ResponseData {
-    data: HospitalLevelAndRegionArr;
+  data: HospitalLevelAndRegionArr;
 }
 
 // 医院信息-搜索
 export interface HospitalInfoSearch extends ResponseData {
-    data: HospitalList
+  data: HospitalList;
 }
--- src/api/hospital/index.ts
+++ src/api/hospital/index.ts
@@ -1,6 +1,12 @@
 import request from '@/utils/request';
+import type { HospitalDetailResponse } from '@/api/hospital/type';
 
 enum API {
   // GET /api/hosp/hospital/{hoscode}
   HOSPITALDETAIL_URL = '/hosp/hospital/',
 }
+
+// 请求医院详情
+export const reqHospitalDetail = (hoscode: string) => {
+  return request.get<any, HospitalDetailResponse>(API.HOSPITALDETAIL_URL + hoscode);
+};
