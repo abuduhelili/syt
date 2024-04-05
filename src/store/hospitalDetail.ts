14121f30e718930702ac96b8e1354b3a484faa1c
harry <2062454635@qq.com>
2024-04-06 01:02:03 +0800

    style

--- src/store/hospitalDetail.ts
+++ src/store/hospitalDetail.ts
@@ -1,19 +1,20 @@
 // 创建仓库
-import { defineStore } from 'pinia';
-import { reqHospitalDetail } from '@/api/hospital';
-import type { HospitalDetail, HospitalDetailResponse } from '@/api/hospital/type';
-import { ref } from 'vue';
+import {defineStore} from 'pinia';
+import {reqHospitalDetail} from '@/api/hospital';
+import type {HospitalDetail, HospitalDetailResponse} from '@/api/hospital/type.ts';
+import {ref} from 'vue';
 
 export const useHospitalDetail = defineStore('hospitalDetail', () => {
-  const hospitalDate = ref<HospitalDetail | null>(null);
-  async function getHospital(hoscode: string) {
-    const result: HospitalDetailResponse = await reqHospitalDetail(hoscode);
-    if (result.code === 200) {
-      // console.log(result);
-      hospitalDate.value = result.data;
-      // console.log(hospitalDate.value);
-    }
-  }
+    const hospitalDate = ref<HospitalDetail | null>(null);
 
-  return { hospitalDate, getHospital };
+    async function getHospital(hoscode: string) {
+        const result: HospitalDetailResponse = await reqHospitalDetail(hoscode);
+        if (result.code === 200) {
+            // console.log(result);
+            hospitalDate.value = result.data;
+            // console.log(hospitalDate.value);
+        }
+    }
+
+    return {hospitalDate, getHospital};
 });
